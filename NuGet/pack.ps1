d2080888f27336c41237aa60a1eb6358ad03fa12
Martin Kuschnik <martin-kuschnik@live.com>
2015-09-05 20:43:30 +0200

    * changed NuGet spec file * added NuGet.exe and deployment script

---
+++ NuGet/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\Goji\bin\Release\Goji.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\Goji.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\NuGet\Goji.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\NuGet\Goji.nuspec
