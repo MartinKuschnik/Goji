5aba2ce978e90ca95f0d7f2af9e70186e6e78205
Martin Kuschnik <martin-kuschnik@live.com>
2015-09-07 22:27:15 +0200

    * added som documentations for the RelayCommand class

--- Goji.TestApp/Misc/RelayCommand.cs
+++ Goji.TestApp/Misc/RelayCommand.cs
@@ -3,24 +3,29 @@
     using System;
     using System.Windows.Input;
 
-    public class RelayCommand : ICommand
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality to other objects by invoking delegates. 
+    /// <para />
+    /// The default return value for the CanExecute method is <c>true</c>.
+    /// </summary>
+    internal sealed class RelayCommand : ICommand
     {
+        /// <summary>
+        /// The action which will be executed when the <see cref="ICommand.Execute(object)"/> method is called.
+        /// </summary>
+        private readonly Action<object> execute;
 
-        #region Events
+        /// <summary>
+        /// The action which will be executed when the <see cref="ICommand.CanExecute(object)"/> method is called.
+        /// </summary>
+        private readonly Predicate<object> canExecute;
 
-        public event EventHandler CanExecuteChanged;
-
-        #endregion
-
-        #region Fields
-
-        readonly Action<object> execute;
-        readonly Predicate<object> canExecute;
-
-        #endregion // Fields
-
-        #region Constructors
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The action which will be executed when the <see cref="ICommand.Execute(object)"/> method is called.</param>
+        /// <param name="canExecute">The action which will be executed when the <see cref="ICommand.CanExecute(object)"/> method is called.</param>
+        /// <exception cref="System.ArgumentNullException">execute</exception>
         public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
         {
             if (execute == null)
@@ -31,25 +36,36 @@
             this.canExecute = canExecute;
         }
 
-        #endregion
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
 
-        #region ICommand Members
-
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns><c>true</c> if this command can be executed; otherwise, <c>false</c>.</returns>
         public bool CanExecute(object parameter)
         {
             return canExecute == null ? true : canExecute(parameter);
         }
 
+        /// <summary>
+        /// Raises the can execute changed.
+        /// </summary>
         public void RaiseCanExecuteChanged()
         {
             if (CanExecuteChanged != null) CanExecuteChanged(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
         public void Execute(object parameter)
         {
             execute(parameter);
         }
-
-        #endregion
     }
 }
